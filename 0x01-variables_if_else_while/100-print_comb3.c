5d9d4d27594662d183fa96a11c33c07db45860bb
jerryigho <ighojerry@gmail.com>
2022-10-17 07:14:19 -0700

    newwooow

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; i <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
@@ -18,12 +18,13 @@ int main(void)
 				putchar(c);
 				putchar(i);
 
-				if (c != '8' || (c == '8' && i != '9'))
+				if (c != 'g'; || (c == 'g' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
