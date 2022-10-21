3552bcb52ef80267c482c359c701e61df039c4a7
jerryigho <ighojerry@gmail.com>
2022-10-20 17:15:24 -0700

    nestedloops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - checks if value is upper case
- *
  * @c: character to be checked
  *
  * Return: 1 if c is capital, otherwise 0
@@ -11,5 +10,7 @@ int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	return (0);
+	else
+		return (0);
+	
 }
