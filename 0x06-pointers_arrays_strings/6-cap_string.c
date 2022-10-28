18a973173d99b9cd368a51cee54f7e6e9cfca115
jerryigho <ighojerry@gmail.com>
2022-10-28 15:39:04 -0700

    newcorrection

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *s)
 				*(s + i) -= 'a' - 'A';
 			else
 			{
-				*(s + i) -= 'a' - 'A';
+				for (j = 0; j <= 12; j++)
 				{
 					if (a[j] == *(s + i - 1))
 						*(s + i) -= 'a' - 'A';
